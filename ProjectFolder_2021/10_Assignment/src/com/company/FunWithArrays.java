9ab6a3c23ee8122b8f19947114108e282b1387a3
Isaac Gorman <isaacgorman@Isaacs-MBP.lan>
2021-04-09 13:14:43 -0500

    rep 10 on the way!

---
+++ ProjectFolder_2021/10_Assignment/src/com/company/FunWithArrays.java
@@ -0,0 +1,16 @@
+//------------------------------------------------------//
+// Programmer:    Isaac Gorman Uribe
+// Course:        COSC 1336 Section 006/009
+// Semester:      Spring 2021
+// Assignment#:   Assignment 10
+// DueDate:       May 5th, 2021 @11:59 pm
+// -----------------------------------------------------
+
+package com.company;
+
+public class FunWithArrays {
+
+    public static void main(String[] args) {
+
+    } // end main method
+} // end FunWithArrays class
