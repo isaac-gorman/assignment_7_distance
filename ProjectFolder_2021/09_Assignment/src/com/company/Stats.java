d46ccf6ef44c8ad7d8316864693d213646bc311a
Isaac Gorman <isaacgorman@Isaacs-MBP.lan>
2021-04-08 23:02:32 -0500

    prog 1!

--- ProjectFolder_2021/09_Assignment/src/com/company/Stats.java
+++ ProjectFolder_2021/09_Assignment/src/com/company/Stats.java
@@ -20,20 +20,53 @@ public class Stats {
         int[] numbers = new int[MAX_COUNT];
         int i, rangeRandomNumbers;
         float mean, std;
-        String rangeRandomNumbersText, output;
+        String rangeRandomNumbersText, output = "";
 
             // Step 3) Instantiate objects for number generating,
             // and for formatting numbers, respectively;
+         Random gen = new Random();
+         DecimalFormat fmt = new DecimalFormat("0.##");
 
             // Step 4) Prompt the user for a number that will be
             // used as a range of numbers to be generated randomly
             // then read it in and convert it to an 'int'
+        rangeRandomNumbersText =
+                JOptionPane.showInputDialog(null,
+                        "Please enter a range of numbers " +
+                        " to be generated:");
+        rangeRandomNumbers = Integer.parseInt(rangeRandomNumbersText);
 
             // Step 5) Generate 'MAX_COUNT' numbers whose values will be
             // between 0 and 'rangeRandomNumbers - 1' and insert them into
             // the elements of the array 'numbers'
+        for (i = 0; i < MAX_COUNT; i++) {
+            numbers[i] = gen.nextInt(rangeRandomNumbers);
+        } // end for loop
 
             // Step 6) Build up a string to display numbers of array 'numbers'
+        for (i = 0; i < numbers.length; i++) {
+                // format nicely
+            if (i % 2 == 0) { // even number element
+                              // display on beginning of new line
+                              // separate next display by three blank spaces
+                output += "Element " + i +
+                          "has : " + numbers[i] +
+                          "   ";
+            } // end if statement
+            else { // odd number element, display on the same line
+                   // but then advance to the new line at end
+                output += "Element " + i +
+                        "has : " + numbers[i] +
+                        "\n";
+            } // end else statement
+        } // end for loop
+
+            // display the numbers form the array "numbers"
+        JOptionPane.showMessageDialog(null,
+                output,
+                "Your array with the generated numbers",
+                JOptionPane.INFORMATION_MESSAGE);
+
 
             // Step 7.1) Invoke the static method 'calculateMean' to obtain
             // the mean of the numbers
